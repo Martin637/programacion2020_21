bbcb776f6dd7a7d4156fbde9fb9e686921a71c05
manuel molino <programacionpsdam@gmail.com>
2020-10-27 14:44:17 +0100

    borrado:        estructurasControl/teoria/IteradorWhile2.java 	nuevo archivo:  estructurasControl/teoria/Iteradores.java 	nuevo archivo:  estructurasControl/teoria/Multiplos.java

---
+++ estructurasControl/teoria/Multiplos.java
@@ -0,0 +1,28 @@
+/*
+ El programa dado dos números, lo que haces es lo siguiente
+ * paso 10 y 2 y muestra los múltiplos de 2 que van desde 0  a 10
+ * 0 2 4 6 8 10
+ * paso 10 y 3 muestra los múltiplos de 3
+ * 0 3 6 9
+ * paso 10 y 4 muestra múltiplos de 4
+ * 0 4 8
+ * paso 10 y 7 muestra múltiplos de 7
+ * 0 7
+ */
+
+
+public class Multiplos {
+	
+	public static void main (String[] args) {
+		
+	}
+	
+	//métodos void
+	public static void mostrarMultiplos (int numero, int divisor) {
+		//usando while
+		//código que mostrar en consola
+	}
+	
+	//otros dos métodos usando do-while y usando for
+}
+
