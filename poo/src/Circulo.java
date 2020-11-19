a38480c5f8208c0f244472db64ee6b9759176805
manuel molino <programacionpsdam@gmail.com>
2020-11-19 12:43:53 +0100

    modificado:     .gitignore 	nuevo archivo:  poo/.idea/$PRODUCT_WORKSPACE_FILE$ 	nuevo archivo:  poo/.idea/.gitignore 	nuevo archivo:  poo/.idea/misc.xml 	nuevo archivo:  poo/.idea/modules.xml 	nuevo archivo:  poo/.idea/vcs.xml 	nuevo archivo:  poo/diagramas/circulo.dia 	nuevo archivo:  poo/diagramas/circulo.png 	nuevo archivo:  poo/poo.iml 	nuevo archivo:  poo/src/Circulo.java 	nuevo archivo:  poo/src/Circulo1.java 	nuevo archivo:  poo/src/Rectangulo.java 	nuevo archivo:  poo/src/TestCirculo.java 	nuevo archivo:  poo/src/TestRectangulo.java

---
+++ poo/src/Circulo.java
@@ -0,0 +1,26 @@
+public class Circulo {
+    //atributo
+    private float radio;
+    //constructor
+    public Circulo (float radio) {   //new Circulo(4.5)
+        this.radio = radio;
+    }
+
+    //getter
+    public float getRadio () {
+        return radio;
+    }
+    //setter
+    public void setRadio (float radio) {
+        this.radio = radio;
+    }
+    //calcular el perímetro
+    public float getLongitud () {
+        return (float) (2 * 3.14 * radio);
+    }
+    //calcular área
+    public float getArea () {
+        return 3.14f * radio * radio;
+    }
+
+}
