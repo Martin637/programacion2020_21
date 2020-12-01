4327f723dca042fa63a1d7fc7ddd79414bc60bf4
manuel molino <programacionpsdam@gmail.com>
2020-12-01 14:43:39 +0100

    nuevo archivo:  examenes/src/primeraEvaluacion/Digitos.java 	nuevo archivo:  poo/src/ejercicios/EcuacionSegundoGrado.java 	nuevo archivo:  poo/src/ejercicios/TestEcuacioneSegundoGrado.java 	nuevo archivo:  poo/src/ejercicios/TestTrianguloRectangulo.java 	nuevo archivo:  poo/src/ejercicios/TrianguloRectangulo.java

---
+++ poo/src/ejercicios/TrianguloRectangulo.java
@@ -0,0 +1,17 @@
+package ejercicios;
+
+public class TrianguloRectangulo {
+    private double cateto1;
+    private double cateto2;
+
+    public TrianguloRectangulo(double cateto1, double cateto2) {
+        this.cateto1 = cateto1;
+        this.cateto2 = cateto2;
+    }
+    //métodos calcular hipotenusa, área y perímetro
+    @Override
+    public String toString() {
+        //devolver cadena formateada con los catetos, hipotenusa, área y perímetro
+        return null;
+    }
+}
