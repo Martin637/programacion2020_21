902a2cd6169766b3f49cc161737a984376314194
manuel molino <programacionpsdam@gmail.com>
2021-02-01 12:44:48 +0100

    nuevo archivo:  cadenas/UML/PalabraLeida.dia 	nuevo archivo:  cadenas/UML/PalabraLeida.png 	nuevo archivo:  cadenas/src/ejercicios/ejercicio2/ClaveSegura.java 	nuevo archivo:  cadenas/src/ejercicios/ejercicio2/TestClave.java

---
+++ cadenas/src/ejercicios/ejercicio2/TestClave.java
@@ -0,0 +1,28 @@
+package ejercicios.ejercicio2;
+
+import java.util.Random;
+
+public class TestClave {
+    public static void main(String[] args) {
+
+        ClaveSegura claveSegura = new ClaveSegura();
+        //si posibleClave es una clave segura, llamando al método esClaveSegura de la clase ClaveSegura
+        //Creo un objeto ClaveSegura, donde el atributo claveSegura sería posibleClave
+        /*if (ClaveSegura.esClaveSegura(posibleClave)) {
+            ClaveSegura claveSegura = new ClaveSegura();
+            claveSegura.setClaveSegura(posibleClave);
+            System.out.printf("Clave segura %s%n", claveSegura.getClaveSegura());
+        }*/
+        while (true) {
+            String posibleClave = ClaveSegura.getPosibleClave();
+            if (ClaveSegura.esClaveSegura(posibleClave)) {
+                claveSegura.setClaveSegura(posibleClave);
+                break;
+            }
+        }
+        System.out.printf("Clave segura %s%n", claveSegura.getClaveSegura());
+
+    }
+
+
+}
